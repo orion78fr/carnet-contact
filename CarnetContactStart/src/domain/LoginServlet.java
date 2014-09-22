d567efa1d9ef74d83c0e9e8ef8258a7034cd416d
TURCHINI GUILLAUME <orion78fr@gmail.com>
2014-09-22 14:06:50 +0200

    TP1 exo 1

---
+++ CarnetContactStart/src/domain/LoginServlet.java
@@ -0,0 +1,42 @@
+package domain;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ConnectServlet
+ */
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		if(request.getParameter("name").equals(request.getParameter("password"))){
+			RequestDispatcher dispatcher = request.getRequestDispatcher("hello.jsp");
+			dispatcher.forward(request, response);
+		} else {
+			response.sendRedirect("index.html");
+		}
+	}
+
+}
