aad60a571567c490e465ad0fade5ee236f843074
Guillaume Turchini <orion78fr@gmail.com>
2014-11-25 13:32:34 +0100

    Ajout de la sécurité pour la suppression des groupes (comme pour les contacts)

--- CarnetContactStart/src/mbeans/DelGroup.java
+++ CarnetContactStart/src/mbeans/DelGroup.java
@@ -3,16 +3,36 @@ package mbeans;
 import java.io.Serializable;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 
+import codel.ContactGroup;
 import service.ServiceGroup;
 
 @ManagedBean
+@ViewScoped
 public class DelGroup implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private long id;
 	
-	public String delGroup(long id){
-		this.id = id;
+	private ContactGroup group;
+	
+	public DelGroup(){
+		String idc = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("idGroup");
+		if (idc == null) return;
+		this.id = Long.parseLong(idc);
+		this.group = ServiceGroup.getGroup(id);
+	}
+	
+	public ContactGroup getGroup() {
+		return group;
+	}
+
+	public void setGroup(ContactGroup group) {
+		this.group = group;
+	}
+
+	public String delGroup(){
 		ServiceGroup.delContactGroup(this.id);
 		// TODO: ajouter un hmessage de succes / erreur
 		return "modifyGroups";
